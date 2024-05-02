cab080d18d69cc9116e5d7bd1c10be5d130ba427
Danil Panarin <panarin.danil.08@gmail.com>
2024-05-02 23:15:47 +0500

    Добавьте файлы проекта.

---
+++ FitnessCenter/WelcomeForm.Designer.cs
@@ -0,0 +1,157 @@
+﻿using System.Drawing;
+using System.Windows.Forms;
+
+namespace FitnesCenter
+{
+    partial class WelcomeForm
+    {
+        /// <summary>
+        ///  Required designer variable.
+        /// </summary>
+        private System.ComponentModel.IContainer components = null;
+
+        /// <summary>
+        ///  Clean up any resources being used.
+        /// </summary>
+        /// <param name="disposing">true if managed resources should be disposed; otherwise, false.</param>
+        protected override void Dispose(bool disposing)
+        {
+            if (disposing && (components != null))
+            {
+                components.Dispose();
+            }
+            base.Dispose(disposing);
+        }
+
+        #region Windows Form Designer generated code
+
+        /// <summary>
+        ///  Required method for Designer support - do not modify
+        ///  the contents of this method with the code editor.
+        /// </summary>
+        private void InitializeComponent()
+        {
+            this.pictureBox1 = new System.Windows.Forms.PictureBox();
+            this.titleLabel2 = new System.Windows.Forms.Label();
+            this.loginTextBox1 = new System.Windows.Forms.TextBox();
+            this.passwordTextBox2 = new System.Windows.Forms.TextBox();
+            this.autorizeButton1 = new System.Windows.Forms.Button();
+            this.titleLabel1 = new System.Windows.Forms.Label();
+            this.splitter = new System.Windows.Forms.Panel();
+            ((System.ComponentModel.ISupportInitialize)(this.pictureBox1)).BeginInit();
+            this.SuspendLayout();
+            // 
+            // pictureBox1
+            // 
+            this.pictureBox1.Image = global::FitnessCenter.Properties.Resources.res1;
+            this.pictureBox1.Location = new System.Drawing.Point(20, 56);
+            this.pictureBox1.Margin = new System.Windows.Forms.Padding(4, 3, 4, 3);
+            this.pictureBox1.Name = "pictureBox1";
+            this.pictureBox1.Size = new System.Drawing.Size(348, 330);
+            this.pictureBox1.TabIndex = 2;
+            this.pictureBox1.TabStop = false;
+            // 
+            // titleLabel2
+            // 
+            this.titleLabel2.AutoSize = true;
+            this.titleLabel2.Font = new System.Drawing.Font("Safira March Personal Use", 22.2F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.titleLabel2.ForeColor = System.Drawing.Color.FromArgb(((int)(((byte)(114)))), ((int)(((byte)(93)))), ((int)(((byte)(89)))));
+            this.titleLabel2.Location = new System.Drawing.Point(464, 42);
+            this.titleLabel2.Margin = new System.Windows.Forms.Padding(4, 0, 4, 0);
+            this.titleLabel2.Name = "titleLabel2";
+            this.titleLabel2.Size = new System.Drawing.Size(149, 90);
+            this.titleLabel2.TabIndex = 3;
+            this.titleLabel2.Text = "XFit";
+            // 
+            // loginTextBox1
+            // 
+            this.loginTextBox1.BorderStyle = System.Windows.Forms.BorderStyle.FixedSingle;
+            this.loginTextBox1.Font = new System.Drawing.Font("TT Commons", 9F);
+            this.loginTextBox1.Location = new System.Drawing.Point(432, 139);
+            this.loginTextBox1.Margin = new System.Windows.Forms.Padding(4, 3, 4, 3);
+            this.loginTextBox1.Name = "loginTextBox1";
+            this.loginTextBox1.Size = new System.Drawing.Size(208, 25);
+            this.loginTextBox1.TabIndex = 4;
+            this.loginTextBox1.TextAlign = System.Windows.Forms.HorizontalAlignment.Center;
+            this.loginTextBox1.UseSystemPasswordChar = true;
+            // 
+            // passwordTextBox2
+            // 
+            this.passwordTextBox2.BorderStyle = System.Windows.Forms.BorderStyle.FixedSingle;
+            this.passwordTextBox2.Font = new System.Drawing.Font("TT Commons", 9F);
+            this.passwordTextBox2.Location = new System.Drawing.Point(432, 193);
+            this.passwordTextBox2.Margin = new System.Windows.Forms.Padding(4, 3, 4, 3);
+            this.passwordTextBox2.Name = "passwordTextBox2";
+            this.passwordTextBox2.PasswordChar = '*';
+            this.passwordTextBox2.Size = new System.Drawing.Size(208, 25);
+            this.passwordTextBox2.TabIndex = 5;
+            this.passwordTextBox2.TextAlign = System.Windows.Forms.HorizontalAlignment.Center;
+            // 
+            // autorizeButton1
+            // 
+            this.autorizeButton1.BackColor = System.Drawing.Color.White;
+            this.autorizeButton1.FlatStyle = System.Windows.Forms.FlatStyle.Popup;
+            this.autorizeButton1.Font = new System.Drawing.Font("TT Commons", 9F);
+            this.autorizeButton1.ForeColor = System.Drawing.Color.FromArgb(((int)(((byte)(114)))), ((int)(((byte)(93)))), ((int)(((byte)(89)))));
+            this.autorizeButton1.Location = new System.Drawing.Point(478, 244);
+            this.autorizeButton1.Margin = new System.Windows.Forms.Padding(4, 3, 4, 3);
+            this.autorizeButton1.Name = "autorizeButton1";
+            this.autorizeButton1.Size = new System.Drawing.Size(116, 30);
+            this.autorizeButton1.TabIndex = 6;
+            this.autorizeButton1.Text = "ВОЙТИ";
+            this.autorizeButton1.UseVisualStyleBackColor = false;
+            // 
+            // titleLabel1
+            // 
+            this.titleLabel1.AutoSize = true;
+            this.titleLabel1.Font = new System.Drawing.Font("TTMoons-Bold", 9F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.titleLabel1.ForeColor = System.Drawing.Color.FromArgb(((int)(((byte)(114)))), ((int)(((byte)(93)))), ((int)(((byte)(89)))));
+            this.titleLabel1.Location = new System.Drawing.Point(20, 9);
+            this.titleLabel1.Name = "titleLabel1";
+            this.titleLabel1.Size = new System.Drawing.Size(145, 19);
+            this.titleLabel1.TabIndex = 7;
+            this.titleLabel1.Text = "ФИТНЕС ЦЕНТР XFIT";
+            // 
+            // splitter
+            // 
+            this.splitter.BackColor = System.Drawing.Color.FromArgb(((int)(((byte)(114)))), ((int)(((byte)(93)))), ((int)(((byte)(89)))));
+            this.splitter.Location = new System.Drawing.Point(20, 38);
+            this.splitter.Name = "splitter";
+            this.splitter.Size = new System.Drawing.Size(661, 1);
+            this.splitter.TabIndex = 8;
+            // 
+            // WelcomeForm
+            // 
+            this.AutoScaleDimensions = new System.Drawing.SizeF(8F, 20F);
+            this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
+            this.BackColor = System.Drawing.Color.White;
+            this.ClientSize = new System.Drawing.Size(693, 410);
+            this.Controls.Add(this.splitter);
+            this.Controls.Add(this.titleLabel1);
+            this.Controls.Add(this.autorizeButton1);
+            this.Controls.Add(this.passwordTextBox2);
+            this.Controls.Add(this.loginTextBox1);
+            this.Controls.Add(this.titleLabel2);
+            this.Controls.Add(this.pictureBox1);
+            this.Font = new System.Drawing.Font("Segoe UI", 9F);
+            this.Margin = new System.Windows.Forms.Padding(4, 3, 4, 3);
+            this.MaximizeBox = false;
+            this.MinimizeBox = false;
+            this.Name = "WelcomeForm";
+            this.Text = " ";
+            ((System.ComponentModel.ISupportInitialize)(this.pictureBox1)).EndInit();
+            this.ResumeLayout(false);
+            this.PerformLayout();
+
+        }
+
+        #endregion
+        private PictureBox pictureBox1;
+        private Label titleLabel2;
+        private TextBox loginTextBox1;
+        private TextBox passwordTextBox2;
+        private Button autorizeButton1;
+        private Label titleLabel1;
+        private Panel splitter;
+    }
+}
---
+++ FitnessCenter/WelcomeForm.cs
@@ -0,0 +1,73 @@
+﻿using System.Windows.Forms;
+using System;
+
+namespace FitnesCenter
+{
+    public partial class WelcomeForm : Form
+    {
+        public WelcomeForm()
+        {
+            InitializeComponent();
+        }
+
+        private void loginTextBox1_KeyDown(object sender, KeyEventArgs e)
+        {
+            if (e.KeyCode == Keys.Enter)
+            {
+                e.Handled = true;
+                e.SuppressKeyPress = true;
+                SelectNextControl((Control)sender, true, true, true, true);
+            }
+        }
+
+        private void passwordTextBox2_KeyDown(object sender, KeyEventArgs e)
+        {
+            if (e.KeyCode == Keys.Enter)
+            {
+                e.Handled = true;
+                e.SuppressKeyPress = true;
+                proccesAutorization();
+            }
+        }
+
+        private void autorizeButton1_Click(object sender, EventArgs e)
+        {
+            proccesAutorization();
+        }
+
+        private void proccesAutorization()
+        {
+            if ((loginTextBox1.Text == "й") && (passwordTextBox2.Text == "ц"))
+            {
+                this.Hide();
+                MainForm mainForm = new MainForm();
+                mainForm.Show();
+            }
+            else if ((loginTextBox1.Text.Length == 0) || (passwordTextBox2.Text.Length == 0)) return;
+            else
+            {
+                MessageBox.Show("Неправильный логин или пароль", "Ошибка", MessageBoxButtons.OK, MessageBoxIcon.Information);
+            }
+        }
+
+        private void loginTextBox1_Enter(object sender, EventArgs e)
+        {
+            loginTextBox1.TextAlign = HorizontalAlignment.Left;
+        }
+
+        private void loginTextBox1_Leave(object sender, EventArgs e)
+        {
+            loginTextBox1.TextAlign = loginTextBox1.Text.Length > 0 ? HorizontalAlignment.Left : HorizontalAlignment.Center;
+        }
+
+        private void passwordTextBox2_Enter(object sender, EventArgs e)
+        {
+            passwordTextBox2.TextAlign = HorizontalAlignment.Left;
+        }
+
+        private void passwordTextBox2_Leave(object sender, EventArgs e)
+        {
+            passwordTextBox2.TextAlign = passwordTextBox2.Text.Length > 0 ? HorizontalAlignment.Left : HorizontalAlignment.Center;
+        }
+    }
+}
