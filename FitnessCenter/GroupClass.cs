11f73b0e1ed11dad3fa4eb74046865489f546aaa
Danil Panarin <panarin.danil.08@gmail.com>
2024-05-04 14:44:35 +0500

    Класс для групповых занятий и считывание его из JSON

---
+++ FitnessCenter/GroupClass.cs
@@ -0,0 +1,14 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace FitnessCenter.Resources
+{
+    internal class GroupClass
+    {
+        public string Name { get; set; }
+        public string[] times { get; set; }
+    }
+}
--- FitnessCenter/Manager.cs
+++ FitnessCenter/Manager.cs
@@ -1,4 +1,5 @@
-﻿using Newtonsoft.Json;
+﻿using FitnessCenter.Resources;
+using Newtonsoft.Json;
 using Newtonsoft.Json.Linq;
 using System;
 using System.Collections.Generic;
@@ -9,10 +10,12 @@ namespace FitnessCenter
     internal class Manager
     {
         private List<Client> _clients;
+        private List<GroupClass> _groupClasses;
 
         public Manager()
         {
             _clients = LoadClientsFromJson("data.json");
+            _groupClasses = LoadClassesFromJSON("data.json");
         }
 
         private List<Client> LoadClientsFromJson(string jsonFilePath)
@@ -23,10 +26,23 @@ namespace FitnessCenter
             return JsonConvert.DeserializeObject<List<Client>>(clientsArray);
         }
 
+        private List<GroupClass> LoadClassesFromJSON(string jsonFilePath)
+        {
+            string json = File.ReadAllText(jsonFilePath);
+            var jsonObject = JsonConvert.DeserializeObject<JObject>(json);
+            var classesArray = jsonObject["group_classes"].ToString();
+            return JsonConvert.DeserializeObject<List<GroupClass>>(classesArray);            
+        }
+
 
         public List<Client> GetClients()
         {
             return _clients;
+        }
+
+        public List<GroupClass> GetClasses()
+        {
+            return _groupClasses;
         }
     }
 }
