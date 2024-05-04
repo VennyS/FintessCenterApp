9cb21575e457954922bfa4f4d9358614679abca2
Danil Panarin <panarin.danil.08@gmail.com>
2024-05-04 14:30:17 +0500

    Улучшения

---
+++ FitnessCenter/Client.cs
@@ -0,0 +1,38 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace FitnessCenter
+{
+    internal class Client
+    {
+        public string full_name { get; set; }
+        public DateTime date_of_birth { get; set; }
+        public int weight_kg { get; set; }
+        public int height_cm { get; set; }
+        public int remaining_visits { get; set; }
+
+        public Client() { }
+
+        public Client(string full_name, DateTime date_of_birth, int weight_kg, int height_cm, int remaining_visits)
+        {
+            this.full_name = full_name;
+            this.date_of_birth = date_of_birth;
+            this.weight_kg = weight_kg;
+            this.height_cm = height_cm;
+            this.remaining_visits = remaining_visits;
+        }
+
+        public Client(string full_name, DateTime date_of_birth, int weight_kg, int height_cm)
+        {
+            this.full_name = full_name;
+            this.date_of_birth = date_of_birth;
+            this.weight_kg = weight_kg;
+            this.height_cm = height_cm;
+        }
+
+        public void addClasses(int count) { remaining_visits += count; }
+    }
+}
---
+++ FitnessCenter/Manager.cs
@@ -0,0 +1,32 @@
+﻿using Newtonsoft.Json;
+using Newtonsoft.Json.Linq;
+using System;
+using System.Collections.Generic;
+using System.IO;
+
+namespace FitnessCenter
+{
+    internal class Manager
+    {
+        private List<Client> _clients;
+
+        public Manager()
+        {
+            _clients = LoadClientsFromJson("data.json");
+        }
+
+        private List<Client> LoadClientsFromJson(string jsonFilePath)
+        {
+            string json = File.ReadAllText(jsonFilePath);
+            var jsonObject = JsonConvert.DeserializeObject<JObject>(json);
+            var clientsArray = jsonObject["clients"].ToString();
+            return JsonConvert.DeserializeObject<List<Client>>(clientsArray);
+        }
+
+
+        public List<Client> GetClients()
+        {
+            return _clients;
+        }
+    }
+}
